fed3145ccba1aea262dd98af26e92f4fa236ac82
lynme <lynmeig@live.com>
2020-11-19 23:59:32 +0800

    增加日志,发送链路追踪信息到zipkin

--- src/main/java/com/lyn/provider/controller/HelloController.java
+++ src/main/java/com/lyn/provider/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.lyn.provider.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Value("${provider.name}")
     private String name;
@@ -18,6 +20,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         System.out.println("some one call me");
+        logger.info("here is the log");
         return "provider:" + name + " port: " + port;
     }
 }
